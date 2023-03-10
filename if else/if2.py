7d9b5507a2afac44574b2cec6ff6a72f9d795b0b
diegoodss <112525573+diegoodss@users.noreply.github.com>
2023-03-09 20:01:39 -0400

    Add files via upload

---
+++ if else/if2.py
@@ -0,0 +1,9 @@
+idade=int(input('Digite sua idade '))
+if idade ==16:
+    print('Pode votar ')
+else:
+    if idade>=16:
+        print('Pode dirigir ')
+    else:
+         if idade <16:
+             print('apenas estude')
